e668e2e19d8a39616fb1d2c430eaf268285f1032
Soohyun Lim <d2axasap@gmail.com>
2021-01-26 23:49:04 -0500

    Converted Map component from class component to functional component. Moved array variables to outside of Search component.

--- client/src/components/map.jsx
+++ client/src/components/map.jsx
@@ -2,53 +2,40 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { MAP_API } from '../../utils/googleConfig.js';
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleCarClick = this.handleCarClick.bind(this);
-  }
+const Map = ({ defaultCenter, zoom, center, cars, getClickedCar }) => (
+  <div style={{ height: '500px', width: '100%' }}>
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: MAP_API, language: 'en' }}
+      defaultCenter={defaultCenter}
+      defaultZoom={zoom}
+      center={center}
+      onChildClick={e => getClickedCar(e)}
+    >
+      {cars.map((car, idx) => {
+        return (
+          <Car
+            style={{
+              position: 'absolute',
+              transform: 'translate(-50%, -50%)'
+            }}
+            lat={car.latitude}
+            lng={car.longitude}
+            key={idx}
+          />
+        );
+      })}
+    </GoogleMapReact>
+  </div>
+);
 
-  handleCarClick(idx) {
-    this.props.getClickedCar(idx);
-  }
-
-  render() {
-    const { defaultCenter, zoom, center, cars } = this.props;
-
-    return (
-      <div style={{ height: '500px', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: MAP_API, language: 'en' }}
-          defaultCenter={defaultCenter}
-          defaultZoom={zoom}
-          center={center}
-          onChildClick={e => this.handleCarClick(e)}
-        >
-          {cars.map((car, idx) => {
-            return (
-              <Car
-                style={{
-                  position: 'absolute',
-                  transform: 'translate(-50%, -50%)'
-                }}
-                lat={car.latitude}
-                lng={car.longitude}
-                text={<img
-                  src="./car.png"
-                  width="25px"
-                  height="25px"
-                />}
-                key={idx}
-              />
-            );
-          })}
-        </GoogleMapReact>
-      </div>
-    )
-  }
-};
-
-const Car = ({ text }) => <div>{text}</div>;
+const Car = () => (
+  <div>
+    <img
+      src="./car.png"
+      width="25px"
+      height="25px"
+    />
+  </div>
+);
 
 export default Map;
--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -5,6 +5,18 @@ import parseDistance from '../../utils/parseDistance.js';
 import getMileageMinAndMax from '../../utils/getMileageMinAndMax.js';
 import { GEO_API } from '../../utils/googleConfig.js';
 
+const distances = [
+  '5 Miles from', '10 Miles from', '20 Miles from', '30 Miles from', '40 Miles from', '60 Miles from', '100 Miles from', '150 Miles from', '250 Miles from', '500 Miles from'
+];
+
+const manufacturers = [
+  'chevrolet', 'bmw', 'ford', 'toyota', 'jeep', 'ram', 'hyundai', 'honda', 'dodge', 'lexus', 'mercedes-benz', 'cadillac', 'gmc', 'subaru', 'infiniti', 'mazda', 'mini', 'nissan', 'volkswagen', 'kia', 'lincoln', 'mitsubishi', 'buick', 'audi', 'rover', 'chrysler', 'fiat', 'acura', 'volvo', 'pontiac', 'tesla', 'saturn', 'mercury', 'datsun', 'porsche', 'jaguar', 'ferrari', 'alfa-romeo', 'harley-davidson', 'land rover', 'aston-martin', 'morgan', 'hennessey'
+];
+
+const mileages = [
+  '0-10,000 mi', '10,001-20,000 mi', '20,001-30,000 mi', '30,001-40,000 mi', '40,001-50,000 mi', '50,001-60,000 mi', '60,001-70,000 mi', '70,001-80,000 mi', '80,001-90,000 mi', '90,001-100,000 mi', '100,001-120,000 mi', '120,001-140,000 mi', '140,001-160,000 mi', '160,001-200,000 mi', '200,000 mi+'
+];
+
 class Search extends React.Component {
   constructor(props) {
     super(props);
@@ -67,12 +79,6 @@ class Search extends React.Component {
   }
 
   render() {
-    const distances = ['5 Miles from', '10 Miles from', '20 Miles from', '30 Miles from', '40 Miles from', '60 Miles from', '100 Miles from', '150 Miles from', '250 Miles from', '500 Miles from'];
-    const manufacturers = [
-      'chevrolet', 'bmw', 'ford', 'toyota', 'jeep', 'ram', 'hyundai', 'honda', 'dodge', 'lexus', 'mercedes-benz', 'cadillac', 'gmc', 'subaru', 'infiniti', 'mazda', 'mini', 'nissan', 'volkswagen', 'kia', 'lincoln', 'mitsubishi', 'buick', 'audi', 'rover', 'chrysler', 'fiat', 'acura', 'volvo', 'pontiac', 'tesla', 'saturn', 'mercury', 'datsun', 'porsche', 'jaguar', 'ferrari', 'alfa-romeo', 'harley-davidson', 'land rover', 'aston-martin', 'morgan', 'hennessey'
-    ];
-    const mileages = ['0-10,000 mi', '10,001-20,000 mi', '20,001-30,000 mi', '30,001-40,000 mi', '40,001-50,000 mi', '50,001-60,000 mi', '60,001-70,000 mi', '70,001-80,000 mi', '80,001-90,000 mi', '90,001-100,000 mi', '100,001-120,000 mi', '120,001-140,000 mi', '140,001-160,000 mi', '160,001-200,000 mi', '200,000 mi+'];
-
     return(
       <React.Fragment>
         {/* {console.log(this.state)} */}
