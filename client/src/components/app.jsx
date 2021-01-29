7074c062352a92b81ec229ffea8bc598768b7d1e
Soohyun Lim <d2axasap@gmail.com>
2021-01-29 18:20:42 -0500

    Converted App component into stateful functional component using react hooks

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,81 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Map from './map.jsx';
 import Search from './search.jsx';
 import CarDetails from './carDetails.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultCenter: {
-        lat: 40.7282,
-        lng: -73.7949
-      },
-      center: {
-        lat: 40.7282,
-        lng: -73.7949
-      },
-      zoom: 8,
-      cars: [],
-      clickedCar: null
-    };
-    this.getCarList = this.getCarList.bind(this);
-    this.getClickedCar = this.getClickedCar.bind(this);
-  }
+const App = () => {
+  const [defaultCenter, setDefaultCenter] = useState({ lat: 40.7282, lng: -73.7949 });
+  const [center, setCenter] = useState({ lat: 40.7282, lng: -73.7949 });
+  const [zoom, setZoom] = useState(8);
+  const [cars, setCars] = useState([]);
+  const [clickedCar, setClickedCar] = useState(null);
 
-  getCarList(car) {
-    console.log(car);
+  const getCarList = (car) => {
     return axios
       .get('/cars', { params: car })
       .then(({ data }) => {
-        this.setState({
-          center: {
-            lat: car.latitudeMin + 7.5, //
-            lng: car.longitudeMin + 7.5 //
-          },
-          cars: data
+        setCenter({
+          lat: car.latitudeMin + 7.5, //
+          lng: car.longitudeMin + 7.5 //
         });
+        setCars(data);
       })
       .catch(err => console.log(err));
-  }
+  };
 
-  getClickedCar(idx) {
-    this.setState({
-      clickedCar: this.state.cars[idx]
-    })
-  }
+  const getClickedCar = (idx) => {
+    setClickedCar(cars[idx]);
+  };
 
-  render() {
-    return(
-      <React.Fragment>
-        <div className='nav'>
-          <img className='navLogo' src='./main.png' />
-          <h1 className='navText'>GoodBuy AutoMart</h1>
+  return(
+    <React.Fragment>
+      <div className='nav'>
+        <img className='navLogo' src='./main.png' />
+        <h1 className='navText'>GoodBuy AutoMart</h1>
+      </div>
+      <div className='app'>
+        <div className='col-2-3'>
+          <Map
+            defaultCenter={defaultCenter}
+            zoom={zoom}
+            center={center}
+            cars={cars}
+            getClickedCar={getClickedCar}
+          />
         </div>
-        <div className='app'>
-          {/* {console.log(this.state)} */}
-          <div className='col-2-3'>
-            <Map
-              defaultCenter={this.state.defaultCenter}
-              zoom={this.state.zoom}
-              center={this.state.center}
-              cars={this.state.cars}
-              getClickedCar={this.getClickedCar}
-            />
-          </div>
-          <div className='col-1-3'>
-            <Search
-              getCarList={this.getCarList}
-            />
-            {this.state.clickedCar ? <CarDetails
-              clickedCar={this.state.clickedCar}
-            /> : null}
-          </div>
+        <div className='col-1-3'>
+          <Search
+            getCarList={getCarList}
+          />
+          {clickedCar ? <CarDetails clickedCar={clickedCar} /> : null}
         </div>
-      </React.Fragment>
-    );
-  }
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default App;
