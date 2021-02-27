fbe38bf7e76ee46a3d0ddbcb5118baca6163f51d
Soohyun Lim <d2axasap@gmail.com>
2021-02-27 05:30:08 -0500

    Updated server

--- server/index.js
+++ server/index.js
@@ -10,15 +10,7 @@ const app = express();
 
 app.use(compression());
 app.use(bodyParser.json());
-
-if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static('client/dist', { maxAge: '1d' }));
-  app.use(express.static('./client/dist'));
-  // app.get('/', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  // })
-}
-// app.use(express.static('client/dist'));
+app.use(express.static('client/dist'));
 
 app.use('/', router);
 
