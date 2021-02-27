dd2178a7125543749102a0356ee0f9c86ffd12ca
Soohyun Lim <d2axasap@gmail.com>
2021-02-27 04:12:28 -0500

    Modified plugins

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,12 @@ const config = {
     ]
   },
   plugins: [
-    // new webpack.DefinePlugin({
-    //   'process.env.NODE_ENV': JSON.stringify('production')
-    // })
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
   ],
 }
 
