dd1c4fa539ecd4d82341f62c10ee93d742513e7a
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 17:53:58 -0500

    Modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 // call dotenv and it will return an Object with a parsed key
 const env = dotenv.config().parsed || {};
+env['TEST'] = 'come on!';
+env['REACT_APP_TEST'] = 'come!!';
 
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
