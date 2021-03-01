848b5df6badced54fe4752da15d693c3ab5c9dad
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 17:40:23 -0500

    Changed default value of env variable in webpack to empty object

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path'); // to get the current path
 const dotenv = require('dotenv');
 
 // call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
+const env = dotenv.config().parsed || {};
 
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
