79db36d80383f7da808df41bad5b6a42842c83be
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 16:05:53 -0500

    Testing env keys

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -66,8 +66,8 @@ const Search = ({ getCarList }) => {
 
   return(
     <React.Fragment>
-      {/* {process.env.NODE_ENV === 'production' ? console.log('MAP', process.env.MAP_API.slice(0, 10)) : null} */}
       {process.env.NODE_ENV === 'production' ? console.log('HOST', process.env.HOST) : null}
+      {process.env.NODE_ENV === 'production' ? console.log('TEST', process.env.TEST) : null}
       <h1 className='searchByMakeText'>search by make</h1>
       <select
         className='manufacturersDropdown'
