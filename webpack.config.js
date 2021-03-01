173d55ae662171e61750d0c2078384daebeede0a
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 16:48:02 -0500

    Reinstalled dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack');
-const path = require('path');
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
+const path = require('path'); // to get the current path
+const fs = require('fs'); // to check if the file exists
+
 
 // // call dotenv and it will return an Object with a parsed key
 // const env = dotenv.config().parsed;
@@ -10,6 +12,25 @@ const path = require('path');
 //   prev[`process.env.${next}`] = JSON.stringify(env[next]);
 //   return prev;
 // }, {});
+
+
+// Get the root path (assuming your webpack config is in the root of your project!)
+const currentPath = path.join(__dirname);
+
+// Create the fallback path (the production .env)
+const basePath = currentPath + '/.env';
+
+// Check if the file exists, otherwise fall back to the production .env
+const finalPath = fs.existsSync(basePath);
+
+// Set the path parameter in the dotenv config
+const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+// reduce it to a nice object, the same as before (but with the variables from the file)
+const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  return prev;
+}, {});
 
 const config = {
   mode: 'production',
@@ -38,7 +59,7 @@ const config = {
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
-    // new webpack.DefinePlugin(envKeys)
+    new webpack.DefinePlugin(envKeys)
   ],
 }
 
