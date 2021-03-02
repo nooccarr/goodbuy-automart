19db1728ab86ebf11f66c35b1726d8956df1c798
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 20:08:30 -0500

    Fixed typo in webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ const config = {
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
-    new Dotenv(path: './.env.defaults')
+    new Dotenv({ path: './.env.defaults' })
     // new webpack.DefinePlugin(envKeys)
 
     // new webpack.DefinePlugin({
