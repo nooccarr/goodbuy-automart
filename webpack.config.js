f529e50e3b2de5b0f7657a6d036b21528efcd2bf
Soohyun Lim <d2axasap@gmail.com>
2021-03-01 20:54:57 -0500

    Added defaults option to dotenv.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,11 @@ const path = require('path'); // to get the current path
 const dotenv = require('dotenv-defaults');
 
 // call dotenv and it will return an Object with a parsed key
-const env = dotenv.config().parsed;
+const env = dotenv.config({
+  path: './.env',
+  encoding: 'utf8',
+  defaults: './.env.defaults'
+}).parsed;
 
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
