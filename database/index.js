4facd2a8e003f012bd37d0b4a367e492a28c454d
Soohyun Lim <d2axasap@gmail.com>
2021-03-02 20:58:26 -0500

    Added handleDisconnect function in database index.js to handle connection lost to mysql server

--- database/index.js
+++ database/index.js
@@ -13,14 +13,35 @@ if (process.env.NODE_ENV === 'production') {
   mysqlConfig = require('./mysqlConfig');
 }
 
-const db = mysql.createConnection(mysqlConfig);
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Connected!');
-  }
-});
+const handleDisconnect = () => {
+  // Recreate the connection, since the old one cannot be reused
+  db = mysql.createConnection(mysqlConfig);
+
+  // The server is either down or restarting (takes a while sometimes)
+  db.connect((err) => {
+    // We introduce a delay before attempting to reconnect, to avoid a hot loop, and to allow our node script to process asynchronous requests in the meantime. If you're also serving http, display a 503 error
+    if (err) {
+      console.log('Error when connecting to database: ', err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log('Connected!');
+    }
+
+  });
+
+  db.on('error', (err) => {
+    console.log('Database error: ', err);
+    // Connection to the MySQL server is usually lost due to either server restart, or a connnection idle timeout (the wait_timeout server variable configures this)
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = db;
